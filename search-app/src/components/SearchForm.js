aa1d238e6f9a157770eec740faa85a76544a9caf
mihaipaulstd <mihaipaulstd@gmail.com>
2020-02-27 20:50:06 +0200

    installed node-sass and added some basic components

---
+++ search-app/src/components/SearchForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  render() {
+    return (
+      <form>
+        
+      </form>
+    )
+  }
+
+}
+
+export default SearchForm;
