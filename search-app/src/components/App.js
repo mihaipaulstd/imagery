5dfbade9b3ecc0d6a77d5bdc0af356e5c8bcf77d
mihaipaulstd <mihaipaulstd@gmail.com>
2020-02-29 15:21:29 +0200

    added photo modal in full resolution

--- search-app/src/components/App.js
+++ search-app/src/components/App.js
@@ -1,47 +1,51 @@
 import React, { Component } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-
-
-import SearchForm from './SearchForm';
-import ImageCard from './ImageCard';
-
-import pexels from '../api/pexels';
 import Masonry from 'masonry-layout';
 import imagesLoaded from 'imagesloaded';
+import ReactModal from 'react-modal';
+
+import ImageCard from './ImageCard';
+import SearchForm from './SearchForm';
+
+import pexels from '../api/pexels';
+
+ReactModal.setAppElement('#root');
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      per_page: 40,
-      page: 1,
-      images: [],
-      currentTerm: null
+      per_load: 30,
+      loads: 1,
+      images: new Array(),
+      currentImage: new Object(),
+      currentTerm: null,
+      showModal: false
     };
     this.submitHandler = this.submitHandler.bind(this);
     this.fetchImagesOnScroll = this.fetchImagesOnScroll.bind(this);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
   componentDidUpdate() {
     const imageContainer = document.querySelector('.imageContainer');
     
-    imagesLoaded(imageContainer, () =>
+    imagesLoaded(imageContainer, () => 
       new Masonry(imageContainer, {
         itemSelector: '.imageCard',
         columnWidth: '.imageCardSizer',
-        percentPosition: true,
-        
+        percentPosition: true
       }).layout()
-    );
-
+    )
   }
 
   submitHandler(term) {
     pexels.get('/v1/search', {
       params: {
         query: term,
-        per_page: this.state.per_page,
-        page: this.state.page
+        per_page: this.state.per_load,
+        page: this.state.loads
       }
     })
       .then(object => {
@@ -56,20 +60,31 @@ class App extends Component {
     pexels.get('/v1/search', {
       params: {
         query: this.state.currentTerm,
-        per_page: this.state.per_page,
-        page: this.state.page + 1
+        per_page: this.state.per_load,
+        page: this.state.loads + 1
       }
     })
       .then(object => {
-        console.log(this.state)
         this.setState({
-          images: this.state.images.concat(object.data.photos),
-          page: this.state.page + 1
+          // images: this.state.images.concat(object.data.photos),
+          images: [...this.state.images, ...object.data.photos],
+          loads: this.state.loads + 1
         })
       });
   }
 
+  handleOpenModal({ imageProperties }) {
+    this.setState({
+      showModal: true,
+      currentImage: imageProperties
+    })
+
+  }
   
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <div className="app container">
@@ -78,21 +93,39 @@ class App extends Component {
           className="imageContainer"
           dataLength={ this.state.images.length }
           next={ this.fetchImagesOnScroll }
-          scrollThreshold={ 0.8 }
+          scrollThreshold={ 0.7 }
           hasMore={ true }
           loader={ <div /> }
         >
 
           <div className="imageCardSizer"></div>
 
-
           {this.state.images.map((image, index) =>
             <ImageCard
               key={ image.id }
               opacityDelay={ 75 * index }
-              src={ image.src.large } 
+              src={ image.src.large }
+              imageProperties={ image }
+              triggerModal={ this.handleOpenModal }
             />
           )}
+
+          <ReactModal 
+            isOpen={ this.state.showModal }
+            contentLabel="onRequestClose"
+            onRequestClose={ this.handleCloseModal }
+            className="Modal"
+            overlayClassName="Overlay"
+          >
+              <img
+                src={
+                  typeof this.state.currentImage.src !== 'undefined'
+                  ? this.state.currentImage.src.original
+                  : '' 
+                }
+              >
+              </img>
+          </ReactModal>
         </InfiniteScroll>
       </div>
     )
--- search-app/src/components/ImageCard.js
+++ search-app/src/components/ImageCard.js
@@ -12,6 +12,10 @@ class ImageCard extends Component {
     this.imageRef = React.createRef();
 
     this.show = this.show.bind(this);
+    this.hide = this.hide.bind(this);
+    this.focus = this.focus.bind(this);
+    this.blur = this.blur.bind(this);
+    this.triggerModal = this.triggerModal.bind(this);
 
   }
 
@@ -23,14 +27,30 @@ class ImageCard extends Component {
     setTimeout(() => {
       this.focus();
     }, this.props.blurDelay)
+
+    this.imageRef.current.addEventListener('click', this.triggerModal);
+
   }
 
   show() {
     this.setState({ hidden: false })
   }
 
+  hide() {
+    this.setState({ hidden: true })
+  }
+
   focus() {
     this.setState({ blurred: false })
+  }
+
+  blur() {
+    this.setState({ blurred: true })
+  }
+
+  triggerModal() {
+    this.props.triggerModal({ imageProperties: this.props.imageProperties });
+    
   }
 
 
